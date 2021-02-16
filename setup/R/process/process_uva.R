a68583e988b954da93c330db189933e34797c27c
Garrett Dancik <garrett.dancik@gmail.com>
2021-02-16 17:20:50 -0500

    Add UVA dataset

---
+++ setup/R/process/process_uva.R
@@ -0,0 +1,47 @@
+# process UVA data 
+
+library(GEOquery)
+
+cat('processing uva...\n\n')
+
+rm(list = ls())
+source('functions/get_gene_expression.R')
+source('functions/generate_boxplot.R')
+
+# specify dataset name and set file names appropriately
+ds_name <- 'uva'
+file_expr <- paste0('../../data/processed/', ds_name, '.RData')
+file_clinical <- paste0('../../data/clinical/', ds_name, '.RData')
+
+# load expression and platform data
+
+UVA <- getGEO('GSE37317')
+
+uva.expr <- exprs(UVA[[1]])
+
+load("../../data/platforms/GPL96.RData")
+
+# generate boxplot
+generate_boxplot(uva.expr, 'UVA')
+
+# get gene-level expression values
+uva.expr <- get_expression(uva.expr, GPL96)
+
+### extract clinical data -- limit to 
+GSE37317.p <- pData(UVA[[1]])
+
+keep <- GSE37317.p$characteristics_ch1.2 == "histology: urothelial carcinoma"
+GSE37317.expr = uva.expr[,keep]
+GSE37317.p = GSE37317.p[keep,]
+
+# get stages #
+uva.stage = rep(NA, nrow(GSE37317.p))
+uva.stage[GSE37317.p$characteristics_ch1.1 == "muscle invasion: non-muscle invasive"] <- "nmi"
+uva.stage[GSE37317.p$characteristics_ch1.1 == "muscle invasion: muscle invasive"] <- "mi"
+
+# create clinical data table
+uva_clinical <- data.frame(id = colnames(uva.expr), stage = uva.stage)
+
+# save expression and clinical data
+save(uva.expr, file = file_expr)
+save(uva_clinical, file = file_clinical)
