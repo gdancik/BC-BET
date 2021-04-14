1b82cceb960c5f07ddbf9461900358b15d310ace
Garrett Dancik <garrett.dancik@gmail.com>
2021-04-14 18:29:32 -0400

    Added argument options to create_db

--- setup/R/mysql/create_de_tables.R
+++ setup/R/mysql/create_de_tables.R
@@ -1,68 +1,67 @@
 library(RMariaDB)
-library(AUC)
+suppressPackageStartupMessages(library(AUC))
 
-# Connect to employee database using a configuration file,
-# (see tables 4.1 and 4.2 at 
-#    https://dev.mysql.com/doc/refman/8.0/en/option-files.html)
-con <- dbConnect(MariaDB(), group = "BCBET")
+source('functions.R')
 
-#' Carries out a two-sample t-test for vector 'x'
-#'
-#' @param x A vector of expression values
-#' @param y A vector of pheno values corresponding to 'x'
-#' @param groups A vector of group labels; fc is calculated as
-#'        group2 - group1
-#' @return A matrix containing the FC and p-value from the t-test
-#'          
-diff_expr_t_test1 <- function(x, y, groups) {
-  
-  w <- wilcox.test(x ~y)
-  y2 <- factor(y, levels = groups, labels = 0:1)
-  keep <- !is.na(y2)
-  a <- auc(roc(x[keep],y2[keep]))
-  
-  res <- t.test(x ~ y) 
-  groups <- paste('mean in group', groups)
-  logfc <- res$estimate[groups[2]] - res$estimate[groups[1]]
-  
-  cbind(fc = 2**unname(logfc), pt = res$p.value, auc = a, pw = w$p.value)
+library(argparse)
+parser <- ArgumentParser()
+
+parser$add_argument("--datasets", default="all",
+                    help="comma-separated list of datasets to process [default %(default)s]")
+parser$add_argument("--variables", default="all",
+                   help="comma-separated list of variables to process [default %(default)s]")
+parser$add_argument("--drop", default="no",
+                    help="drop tables specified by --variables? (yes/no) [default %(default)s]")
+parser$add_argument("--replace", default = "no",
+                    help = paste0('replace dataset/variable combinations already in db (yes/no)',
+                                  'if "yes", we first delete rows from db [default %(default)s]'))
+
+args <- parser$parse_args()
+
+#########################################################
+# process arguments and check that arguments are valid
+#########################################################
+
+# check yes/no options
+for (p in c('drop', 'replace')) {
+  if (!args[[p]] %in% c('yes','no')) {
+    stop('argument must be yes/no: ', p)
+  }
 }
 
-#' Carries out a two-sample t-test for each row of 'X' 
-#'
-#' @param X A matrix of expression values
-#' @param y A vector of pheno values corresponding to each row of 'X'
-#' @param groups A vector of group labels; fc is calculated as
-#'        group2 - group1
-#' @return A matrix where each row contains the FC and p-value 
-#'         corresponding to each row of X
-#' 
-diff_expr_t_test <- function(X, y, groups) {
-  res <- apply(X, 1, diff_expr_t_test1, y, groups)
-  res <- t(res)
-  colnames(res) <- c('fc', 'pt', 'auc', 'pw')
-  res
+if (args$drop == 'yes') {
+  args$replace = 'no'
 }
 
-#' Loads data from specified datasets
-#'
-#' @param ds The name of the datasets
-#' @return A list containing expression matrix X and clinical table Y
-#' 
-get_data <- function(ds) {
-  load(paste0('../../data/processed/', ds, '.RData'))
-  load(cfile <- paste0('../../data/clinical/', ds, '.RData'))
-  
-  list(X = get(paste0(ds,'.expr')),
-       Y = get(paste0(ds, '_clinical')))
-  
+# process --datasets
+datasets <- NULL
+if (args$datasets == 'all') {
+  datasets <- Sys.glob('../../data/clinical/*.RData')
+  datasets <- gsub('\\.RData','',basename(datasets))
+} else {
+  datasets <- strsplit(args$datasets, ',')[[1]]
 }
 
+valid <- check_data(datasets)
+if (!all(valid)) {
+  stop('invalid dataset(s) detected: ', 
+       paste(names(valid)[which(!valid)], collapse = ','), '\n')
+}
+
+# process --variables
 # we will carry out DE based on each column of 'variables'
 variables <- data.frame(tumor = c('normal', 'tumor'),
                         grade = c('lg', 'hg'),
                         stage = c('nmi', 'mi')
 )
+
+if (args$variables == 'all') {
+  args$variables <- c('tumor', 'grade', 'stage')
+} else {
+  variables <- dplyr::select(variables, args$variables)
+}
+
+
 
 #############################################################################
 # An example using 500 genes from a single dataset -- delete this when
@@ -74,45 +73,33 @@ variables <- data.frame(tumor = c('normal', 'tumor'),
 # v <- 'stage'
 # 
 # res <- diff_expr_t_test(X[1:100,],Y[[v]],variables[[v]])   
-
-create_table <- function(con, table_name, remove = TRUE) {
-
-  if (remove) {
-    dbRemoveTable(con,table_name, fail_if_missing = FALSE)
-  }
-
-
-  dbCreateTable(con, table_name, row.names = NULL, temporary = FALSE,
-              fields = c(gene = 'varchar(40)',
-                         dataset = 'varchar(40)',
-                         fc = 'double', pt = 'double',
-                         auc = 'double', pw = 'double') 
-              )
-  alt <- "ALTER TABLE"
-  tabName <- table_name
-  addInd <- "ADD INDEX `ind` (`gene`);"
-  
-  dbExecute(con,paste(alt,tabName,addInd, sep=" ", collapse=NULL) )
-  
-}
-
-
-create_table(con, 'tumor')
-create_table(con, 'grade')
-create_table(con, 'stage')
 # 
 # #results <- dbGetQuery(con, 'SELECT * from stage LIMIT 10 ')
 # #View(results)
 
+
+#############################################################################
+# Connect to Database 
+#############################################################################
+
+# Connect todatabase using a configuration file,
+# (see tables 4.1 and 4.2 at 
+#    https://dev.mysql.com/doc/refman/8.0/en/option-files.html)
+con <- dbConnect(MariaDB(), group = "BCBET")
+
+
+#############################################################################
+# Create tables if they do not exist
+#############################################################################
+
+drop <- args$drop == "yes"
+for (v in names(variables)) {
+  create_table(con, v, drop = drop)
+}
 #############################################################################
 # Loop through all datasets and all variables
 #############################################################################
 
-datasets <- Sys.glob('../../data/clinical/*.RData')
-datasets <- gsub('\\.RData','',basename(datasets))
-
-#dataset2 <- c('dfci')
-# iterate over all datasets
 for (ds in datasets) {
 
   cat('loading data from: ', ds, '\n')
@@ -123,12 +110,22 @@ for (ds in datasets) {
   # for each phenotype (e.g., tumor, grade, stage)
   for (v in colnames(variables)) {
   
+    # if we are replacing the data, then we need to delete it
+    if (args$replace == "yes") {
+      statement <- paste0('DELETE FROM ', v, ' where dataset = "', ds, '"')
+      dbExecute(con, statement)
+    } else { # otherwise skip if data exists
+     qry <- paste0('select * from ', v, ' where dataset="',ds,'" limit 1')
+     res <- dbGetQuery(con, qry)
+     if (nrow(res) > 0) {
+       cat('db already contains ', ds, '(', v, ') -- skipping\n' )
+       next
+     }
+    }
+    
     if (!is.null(Y[[v]])) {
       cat('DE analysis for ', ds, ':', v, '\n')
       res <- diff_expr_t_test(X, Y[[v]], variables[[v]])
-      
-      # add data to the appropriate table (e.g.,
-      # stage data should be added to 'stage' table)
       
       dataset <- rep(ds,nrow(res))
       gene <- row.names(res)
@@ -136,7 +133,6 @@ for (ds in datasets) {
       res.df <- data.frame(gene,dataset,res)
       dbAppendTable(con,v, res.df,row.names = NULL)
   
-      
     }
   }
   
