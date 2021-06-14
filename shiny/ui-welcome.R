925cd44c54395dde6d6c5fa5aa5d64a17af75038
Garrett Dancik <garrett.dancik@gmail.com>
2021-06-14 13:42:49 -0400

    reformat ui-welcome

--- shiny/ui-welcome.R
+++ shiny/ui-welcome.R
@@ -1,23 +1,30 @@
 
 userOptions <- list(
 
-  fluidRow(style = 'padding-left:0px',
-  accordion(id = 'accordionOptions',
-            accordionItem(title = 'Set statistical options', status = 'success', collapsed = FALSE,
-    selectInput('measure', 
-                HTML(paste0('Class comparison measure (', 
-                     '<a id = "what-measure">?</a>)')),
-                c('FC' = 'fc', 'AUC' = 'auc'), 
-                selected = 'auc'),
-    selectInput('pvalue',  
-                HTML(paste0('P-value for differential expression')),
-                c('Two-sample t-test' = 'pt', 'Wilcoxon Rank-Sum Test' = 'pw'),
-                selected = 'pw'),
-    selectInput('endpoint', 
-                HTML(paste0('Survival endpoint (', 
+  fluidRow(style = 'margin-left:0px',
+  h4('Statistical parameters'),
+  #accordion(id = 'accordionOptions',
+  #          accordionItem(title = 'Statistical parameters', status = 'success', collapsed = FALSE,
+    fluidRow(
+      column(3,
+                    selectInput('measure', 
+                        HTML(paste0('Class comparison measure (', 
+                                    '<a id = "what-measure">?</a>)')),
+                        c('FC' = 'fc', 'AUC' = 'auc'), 
+                        selected = 'auc')
+            ),column(3,
+                    selectInput('pvalue',  
+                        HTML(paste0('P-value for differential expression')),
+                        c('Two-sample t-test' = 'pt', 'Wilcoxon Rank-Sum Test' = 'pw'),
+                        selected = 'pw')
+            ), column(3,
+                      selectInput('endpoint', 
+                        HTML(paste0('Survival endpoint (', 
                             '<a id = "what-endpoint">?</a>)')),
-                 c('Best Available (DSS, OS, RFS)' = 'ba', 'DSS' = 'dss', 'OS' = 'os', 'RFS' = 'rfs')),
-    hr(class = "blue-button", style="height: 2px"),
+                        c('Best Available (DSS, OS, RFS)' = 'ba', 'DSS' = 'dss', 'OS' = 'os', 'RFS' = 'rfs'))
+                      )
+    ),
+    # hr(class = "blue-button", style="height: 2px"),
     
     bsTooltip('what-measure', placement = 'right',
               'Select FC for fold change or AUC for area under the receiver operating characteristics curve'),
@@ -26,13 +33,11 @@ userOptions <- list(
               'DSS = disease-specific survival; OS = overall survival; RFS = recurrence-free survival; Best Available selects the first available endpoint in the list: DSS, OS, RFS'
               )
     
-  ))
+  #))
   )
 )
 
-
-
-userGenes <- list(
+userSingleGene <- list(
   
   h4('Select a gene'),
   HTML("<p>Note: <i>BC-BET</i> uses official gene symbols. Check your genes or lookup invalid symbols from 
@@ -41,7 +46,7 @@ userGenes <- list(
   fluidRow(
     column(3, style="padding-right:0px",
            selectizeInput("geneInput", label = "Select a gene (begin typing for more)", 
-                          choices = sort(c("", "TP53", "HRAS", "CD24", "FGFR3")))
+                          choices = sort(c("", "FPR1", "NKG2A", "CIITA", "TP53", "HRAS", "CD24", "FGFR3")))
     ),
     column(2,style = "vertical-align:middle; padding-left:0px",
            HTML("<label class = 'control-label' style='visibility:hidden'>Hello</label>"),
@@ -50,9 +55,13 @@ userGenes <- list(
                           style = "width:100%;")
            )
     )
-  ), 
+  )
+)
+
+userGenes <- list(
+  
   fluidRow(
-    h4('- OR- ', style = 'padding-left:15px')
+    h4('Select multiple genes ', style = 'padding-left:15px')
   ),
   fluidRow(
     column(3, style = "padding-right:0px",
@@ -97,12 +106,12 @@ userGenes <- list(
 welcomePage <- list(
 
       h2("Bladder Cancer Biomarker Evaluation Tool (BC-BET)"),
-      p(strong("Instructions:"), "Welcome to the..."),
-        
       hr(class = "blue-button", style="height: 2px"),
       
+      userSingleGene,
+      hr(class = "blue-button", style="height: 2px; margin-top: 5px"),
       userOptions,
-      
+      hr(class = "blue-button", style="height: 2px; margin-top: 5px"),      
       userGenes
 )
 
