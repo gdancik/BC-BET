596e12ee4b86fb9f180658d1fc660754c35ea078
Garrett Dancik <garrett.dancik@gmail.com>
2021-08-11 11:40:46 -0400

    Links open new tabs on About page

--- shiny/ui-about.R
+++ shiny/ui-about.R
@@ -14,7 +14,7 @@ tabAbout = tabPanel("About",
       ),
       div(class = "panel-body",
         HTML("<p>If you use <i>BC-BET</i> in your work, please <span style = 'color:red'><b>cite</b></span> the following publication", 
-        "(<a href = 'http://biomedcentral.com/1471-2490/15/59'>link</a>):",
+        "(<a href = 'http://biomedcentral.com/1471-2490/15/59' target = '_blank'>link</a>):",
         "<ul><li><b>Dancik, G.M. An online tool for evaluating diagnostic and prognostic gene 
          expression biomarkers in bladder cancer. <i>BMC Urol</i> 2015, 15:59.</b></li></ul><p>")
         ),
@@ -29,8 +29,8 @@ tabAbout = tabPanel("About",
         HTML("<li>"),
         p(span(style='font-weight:bold', "Garrett M. Dancik, PhD"),
          "is an Associate Professor of Computer Science / ",
-         a(href = "https://gdancik.github.io/bioinformatics/", "Bioinformatics"), "at ",
-         a(href = "https://easternct.edu", "Eastern Connecticut State University"), "(Wilimantic, CT).", 
+         a(href = "https://gdancik.github.io/bioinformatics/", target = '_blank', "Bioinformatics"), "at ",
+         a(href = "https://easternct.edu", "Eastern Connecticut State University", target = '_blank'), "(Wilimantic, CT).", 
          role("Maintainer"),
          role("Contributor")
          ),
@@ -47,7 +47,7 @@ tabAbout = tabPanel("About",
         HTML("<ul>"),
         HTML("<li>"),
         p(span(style='font-weight:bold', "Christopher Kelly"), "graduated from ", 
-          a(href = "https://easternct.edu", "Eastern Connecticut State University"), "(Wilimantic, CT) with a B.S. in Computer Science.", 
+          a(href = "https://easternct.edu", target = '_blank', "Eastern Connecticut State University"), "(Wilimantic, CT) with a B.S. in Computer Science.", 
           role("Contributor")
         ),
         HTML("</li>"),
@@ -59,7 +59,7 @@ tabAbout = tabPanel("About",
            "Acknowledgements")
     ),
     div(class = "panel-body",
-        HTML("<p><i>BC-BET</i> was inspired by postdoctoral work carried out under <a href = 'https://bio.cedars-sinai.org/theodorescud/index.html'>Dan Theodorescu</a>, MD, PhD, who is currently director of the <a href = 'https://www.cedars-sinai.org/locations/samuel-oschin-comprehensive-cancer-institute-150.html'>Samuel Oschin Comprehensive Cancer Center</a> at the Cedars-Sinai Health System in Los Angeles.</li>",
+        HTML("<p><i>BC-BET</i> was inspired by postdoctoral work carried out under <a href = 'https://bio.cedars-sinai.org/theodorescud/index.html' target='_blank'>Dan Theodorescu</a>, MD, PhD, who is currently director of the <a href = 'https://www.cedars-sinai.org/locations/samuel-oschin-comprehensive-cancer-institute-150.html' target='_blank'>Samuel Oschin Comprehensive Cancer Center</a> at the Cedars-Sinai Health System in Los Angeles.</li>",
              "This work was supported, in part, by grants from the American Association of University Professors and Connecticut State University Board of Regents (DANR14 and YRDA01).</p>")
     )
     
