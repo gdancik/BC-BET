01b738ed46058fa5d2abff78516730e5a1a63c1c
Garrett Dancik <garrett.dancik@gmail.com>
2021-08-31 14:38:57 -0400

    Added chen (GSE77952) data

--- setup/R/get_platforms.R
+++ setup/R/get_platforms.R
@@ -54,7 +54,8 @@ wd <- setwdToCurrentFileLocation()
 # get arguments; if no arguments are specified, get all platforms
 args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
-  args <- c('GPL96', 'GPL6102', 'GPL91', 'GPL570', 'GPL4060', 'GPL6947', 'GPL14951')
+  args <- c('GPL96', 'GPL6102', 'GPL91', 'GPL570', 'GPL4060', 'GPL6947', 
+            'GPL14951', 'GPL6480')
 }
 
 for (a in args) {
@@ -68,10 +69,12 @@ for (a in args) {
   } else if (a %in% c('GPL6102', 'GPL6947')){
     x <- dplyr::select(pl, 'ID', 'Symbol') %>%
       dplyr::filter (Symbol != '')
+  } else if (a == 'GPL6480') {
+    x <- dplyr::select(pl, 'ID', 'GENE_SYMBOL') %>%
+      dplyr::filter (GENE_SYMBOL != '')
   } else if (a == 'GPL4060') {
     # map gene symbols to GB ACC using org.Hs.eg.db
     library(org.Hs.eg.db)
-    
     acc <- unique(pl$GB_ACC[pl$GB_ACC != ""])
     s <- AnnotationDbi::select(org.Hs.eg.db, keys = acc, 
                 columns = c("SYMBOL"), keytype = "ACCNUM" )
